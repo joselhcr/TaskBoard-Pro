ade2184458481939dfd6ec99d5339eddaa891f20
joselhcr <josealhcr@gmail.com>
2026-05-06 12:56:25 -0600

    añadiendo el uso del tema

--- taskboard-pro/src/App.jsx
+++ taskboard-pro/src/App.jsx
@@ -1,16 +1,29 @@
 import './App.css'
 import { HTML_TAGS } from './shared/constants/html-tags.constants'
 import { TaskBoard } from './features/tasks/components/TaskBoard'
+import { ThemeToggle } from './features/Theme/components/ThemeToggle'
+import { ThemeProvider } from './features/Theme/context/ThemeProvider'
+import { useTheme } from './features/Theme/hooks/useTheme'
 
-function App() {
+function AppContent() {
   const MainTag = HTML_TAGS.MAIN
   const TitleTag = HTML_TAGS.H1
-  
+const { theme } = useTheme()
+
   return (
     <div>
       <h1>TaskBoard Hooks Lab</h1>
+      <ThemeToggle />
       <TaskBoard />
     </div>
+  )
+}
+
+function App() {
+  return (
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
   )
 }
 
